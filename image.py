355c01ddfb091274b5256a6a627b1a9102558ccc
snarflakes <snarflakes@protonmail.com>
2021-05-02 00:18:34 -0700

    new screen

--- image.py
+++ image.py
@@ -28,7 +28,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import ST7789 as ST7789
+#import ST7789 as ST7789
 
 #audio button code
 import time
@@ -36,6 +36,15 @@ from os import listdir
 import subprocess
 import board
 import digitalio
+
+#new joystick screen
+import time
+import random
+from colorsys import hsv_to_rgb
+import board
+from digitalio import DigitalInOut, Direction
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
 
 
 print("""
@@ -52,42 +61,83 @@ breakout into the front slot.
 #image_file = sys.argv[1]
 
 # Create ST7789 LCD display class.
-disp = ST7789.ST7789(
-    port=0,
-    cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
-    dc=9,
+#disp = ST7789.ST7789(
+#    port=0,
+#    cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
+#    dc=9,
 #    backlight=19,               # 18 for back BG slot, 19 for front BG slot.
-    spi_speed_hz=80 * 1000 * 1000
+#    spi_speed_hz=80 * 1000 * 1000
+#)
+
+# Create the joystick display
+cs_pin = DigitalInOut(board.CE0)
+dc_pin = DigitalInOut(board.D25)
+reset_pin = DigitalInOut(board.D27)
+BAUDRATE = 24000000
+ 
+spi = board.SPI()
+disp = st7789.ST7789(
+    spi,
+    height=240,
+    y_offset=80,
+    rotation=180,
+    cs=cs_pin,
+    dc=dc_pin,
+    rst=reset_pin,
+    baudrate=BAUDRATE,
 )
+
+# Turn on the Backlight, can also use to turn off the backlight for power saving
+backlight = DigitalInOut(board.D24)
+backlight.switch_to_output()
+backlight.value = True
+
 
 WIDTH = disp.width
 HEIGHT = disp.height
 
 
-disp.begin()
+#disp.begin()
 
 #original audio code
 
 #button1=A
-button1 = digitalio.DigitalInOut(board.D5)
+button1 = digitalio.DigitalInOut(board.D21)
 button1.direction = digitalio.Direction.INPUT
 button1.pull = digitalio.Pull.UP
 
 #button2=Y
-button2 = digitalio.DigitalInOut(board.D24)
+button2 = digitalio.DigitalInOut(board.D20)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 
 #button3=X
-button3 = digitalio.DigitalInOut(board.D16)
-button3.direction = digitalio.Direction.INPUT
-button3.pull = digitalio.Pull.UP
+#button3 = digitalio.DigitalInOut(board.D16)
+#button3.direction = digitalio.Direction.INPUT
+#button3.pull = digitalio.Pull.UP
 
 #button4=B
-button4 = digitalio.DigitalInOut(board.D6)
+button4 = digitalio.DigitalInOut(board.D16)
 button4.direction = digitalio.Direction.INPUT
 button4.pull = digitalio.Pull.UP
 
+#direction pad
+button_L = DigitalInOut(board.D5)
+button_L.direction = Direction.INPUT
+button_L.pull = digitalio.Pull.UP
+
+
+button_R = DigitalInOut(board.D26)
+button_R.direction = Direction.INPUT
+
+button_U = DigitalInOut(board.D6)
+button_U.direction = Direction.INPUT
+
+button_D = DigitalInOut(board.D19)
+button_D.direction = Direction.INPUT
+
+button_C = DigitalInOut(board.D13)
+button_C.direction = Direction.INPUT
 
 
 
@@ -124,9 +174,9 @@ while True:
         image_bytes = io.BytesIO(response.content)
         img = PIL.Image.open(image_bytes)
         resized_img = img.resize((WIDTH, HEIGHT))
-        disp.display(resized_img)
+        disp.image(resized_img)
 
-        subprocess.Popen(['omxplayer', '-o', 'alsa', mp3_files[0]])
+#        subprocess.Popen(['omxplayer', '-o', 'alsa', mp3_files[0]])
         time.sleep(0.25)
 
 #        index += 1
@@ -140,20 +190,20 @@ while True:
         image_bytes = io.BytesIO(response.content)
         img = PIL.Image.open(image_bytes)
         resized_img = img.resize((WIDTH, HEIGHT))
-        disp.display(resized_img)
+        disp.image(resized_img)
 
-        subprocess.Popen(['omxplayer', '-o', 'alsa', mp3_files[1]])
+#        subprocess.Popen(['omxplayer', '-o', 'alsa', mp3_files[1]])
 #        print('--- Playing ' + mp3_files[index] + ' ---')
 #        print('--- Press button 3(X) to clear playing mp3s. ---')
         time.sleep(0.25)
 
-    if not button3.value:
-        print("Clear Screen")
+#    if not button3.value:
+#        print("Clear Screen")
 #	clear screen to black
-        img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
-        draw = ImageDraw.Draw(img)
-        disp.display(img)
-        time.sleep(0.25)
+#        img = Image.new('RGB', (WIDTH, HEIGHT), color=(0, 0, 0))
+#        draw = ImageDraw.Draw(img)
+#        disp.display(img)
+#        time.sleep(0.25)
 
     if not button4.value:
         print("Your Third Gan Punk")
@@ -161,8 +211,16 @@ while True:
         image_bytes = io.BytesIO(response.content)
         img = PIL.Image.open(image_bytes)
         resized_img = img.resize((WIDTH, HEIGHT))
-        disp.display(resized_img)
+        disp.image(resized_img)
 
+
+    if not button_L.value:
+        print("Your fourth Gan Punk")
+        response = requests.get("https://lh3.googleusercontent.com/zpZj6rX-zlK7qTvqyDpMUdZy3HHjY1t1QpjQ6mXglYt3pZig1ACkQeA7hW8nyzwQpFA5QCDzHdd61Xy2xKZuvc_bQCfjmTFphUzc=s0") 
+        image_bytes = io.BytesIO(response.content)
+        img = PIL.Image.open(image_bytes)
+        resized_img = img.resize((WIDTH, HEIGHT))
+        disp.image(resized_img)
 
 
 #punks
